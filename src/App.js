9d8c2d83f21be4a9185ce279bfb7d29738a90fe3
ramesh <rameshkumarvrs@gmail.com>
2026-07-03 21:42:11 +0530

    added feed component inside it there is two mmore new components called stories and posts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 
 import './App.css';
+import Feed from './Feed';
 import Sidebar from './Sidebar';
 
 import React from 'react'
+import Suggestions from './Suggestions';
 
 const App = () => {
   return (
     <div className='d-flex vh-100'>
   
      <div className='w-20'><Sidebar/></div>
-     <div className='w-50 bg-info'>feed</div>
-     <div className='w-30'>suggestions</div>
+     <div className='w-50 bg-info'><Feed /></div>
+     <div className='w-30'><Suggestions/></div>
 
 
     </div>
