f7cacd1e1511a18547f53a777174da37918f02cf
ramesh <rameshkumarvrs@gmail.com>
2026-07-03 15:11:31 +0530

    added bootstrap icon for the corresponding nav bars

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -5,20 +5,20 @@ const Sidebar = () => {
     <>
     <div>
         <img className='logo-text' src="instagram-text.png" alt="" />
-        <div>Home</div>
-        <div>Search</div>
-        <div>Explore</div>
-        <div>Reels</div>
-        <div>Messages</div>
-        <div>Notifications</div>
-        <div>Create</div>
-        <div>Profile</div>
+        <div> <i className="bi bi-house-door"></i> Home</div>
+        <div><i className="bi bi-search"></i>Search</div>
+        <div><i class="bi bi-compass-fill"></i>Explore</div>
+        <div><i className="bi bi-file-play"></i>Reels</div>
+        <div><i className="bi bi-chat-dots"></i>Messages</div>
+        <div><i className="bi bi-heart"></i>Notifications</div>
+        <div><i className="bi bi-file-plus"></i>Create</div>
+        <div><i className="bi bi-person-circle"></i>Profile</div>
         
     </div>
 
     <div>
-        <div>Threads</div>
-        <div>More</div>
+        <div><i className="bi bi-threads"></i>Threads</div>
+        <div><i class="bi bi-list"></i>More</div>
     </div>
     
     </>
