d91716b9d60d5786d6a43a809aa7e94631bb133f
ramesh <rameshkumarvrs@gmail.com>
2026-07-03 22:38:14 +0530

    added the margins and padding and align picture in center using default and custom css classes

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -14,16 +14,25 @@ function Posts() {
 
 
   return (
-    <div>
+    <div className='d-flex justify-content-center'>
       {posts.length > 0 ? (
         <div>
            {posts.map((post)=>(
-            <div key={post.id}>
+            <div className='my-3' key={post.id}>
                <div className='d-flex'>
                 <img className="dp rounded-circle" src={post.profilePic} alt="Profile pic" />
                 <h5>{post.username}</h5>
                </div>
-               <img src={post.image} alt="post" />
+               <img className="image" src={post.image} alt="post" />
+               <div>
+                  <i className="bi bi-heart"></i>
+                  <i className="bi bi-chat"></i>
+                  <i className="bi bi-send"></i>
+               </div>
+               <div>
+                  <b>{post.likes} Likes</b>
+                  <p>{post.caption}</p>
+               </div>
             </div>
            ))}
         </div>
