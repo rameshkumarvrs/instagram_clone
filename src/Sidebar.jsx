977d3d667153f05eefc46b0c2bc31bbe02aac6e8
ramesh <rameshkumarvrs@gmail.com>
2026-07-03 16:43:50 +0530

    completed the navbar and similar buttonls like instagram

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -2,26 +2,27 @@ import React from 'react'
 
 const Sidebar = () => {
   return (
-    <>
-    <div className='d-flex flex-column gap-3'>
-        <img className='logo-text' src="instagram-text.png" alt="" />
-        <div> <i className="bi bi-house-door"></i> Home</div>
-        <div><i className="bi bi-search"></i>Search</div>
-        <div><i class="bi bi-compass-fill"></i>Explore</div>
-        <div><i className="bi bi-file-play"></i>Reels</div>
-        <div><i className="bi bi-chat-dots"></i>Messages</div>
-        <div><i className="bi bi-heart"></i>Notifications</div>
-        <div><i className="bi bi-file-plus"></i>Create</div>
-        <div><i className="bi bi-person-circle"></i>Profile</div>
-        
-    </div>
-
-    <div className="position-fixed bottom-0 d-flex flex-column gap-3">
-        <div><i className="bi bi-threads"></i>Threads</div>
-        <div><i class="bi bi-list"></i>More</div>
-    </div>
     
-    </>
+    <div className='m-3'>
+        <div className='d-flex flex-column gap-3'>
+            <img className='logo-text' src="instagram-text.png" alt="" />
+            <div> <i className="bi bi-house-door"></i> Home</div>
+            <div><i className="bi bi-search"></i>Search</div>
+            <div><i class="bi bi-compass-fill"></i>Explore</div>
+            <div><i className="bi bi-file-play"></i>Reels</div>
+            <div><i className="bi bi-chat-dots"></i>Messages</div>
+            <div><i className="bi bi-heart"></i>Notifications</div>
+            <div><i className="bi bi-file-plus"></i>Create</div>
+            <div><i className="bi bi-person-circle"></i>Profile</div>
+            
+        </div>
+
+        <div className="position-fixed bottom-0 d-flex flex-column gap-3 mb-3">
+            <div><i className="bi bi-threads"></i>Threads</div>
+            <div><i class="bi bi-list"></i>More</div>
+        </div>
+    
+    </div>
     
   )
 }
