e6a12e765c9f655f318675a39c9528237784c11f
ramesh <rameshkumarvrs@gmail.com>
2026-07-05 15:40:41 +0530

    added the usestate and use effect for the profile and suggesttions

--- src/Suggestions.jsx
+++ src/Suggestions.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Suggestions = () => {
+
+  const [profile, setProfile] = useState(null)
+  const [suggestions, setSuggestions] = useState([])
+
+  
+  useEffect(()=> {
+    fetch("http://localhost:3002/profile").
+    then(data => data.json()).
+    then(data=> setProfile(data)).
+    catch(err => console.log(err))
+
+     fetch("http://localhost:3002/suggestions").
+    then(data => data.json()).
+    then(data=> setProfile(data)).
+    catch(err => console.log(err))
+
+  },[])
+
   return (
-    <div>Suggestions</div>
+    <div>
+    
+       <div className='d-flex'>
+          <img className="dp rounded-circle" src={post.profilePic} alt="Profile pic" />
+          <h5>{post.username}</h5>
+       </div>
+
+    </div>
   )
 }
 
